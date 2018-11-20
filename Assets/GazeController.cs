c0d58a22400aa3c404e1535437a6ca636204b7cc
Nikolay Ivanov <niki.ivanov98@gmail.com>
2018-11-20 12:47:16 +0100

    Created template for the project
    It contains only a character that can look and move around using the oculus controller and there is also the interface for the main button.

---
+++ Assets/GazeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeController : MonoBehaviour {
+    public void GazeEnter()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void GazeExit()
+    {
+    }
+}
