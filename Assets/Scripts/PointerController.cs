b1965ce092dea7aaee2c83bff8edc067ee724fe6
Nikolay Ivanov <niki.ivanov98@gmail.com>
2018-11-20 23:06:20 +0100

    Added player pointer and ui on hovering elements

---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PointerController : MonoBehaviour
+{
+	public GameObject Timer;
+	private GameObject _lookedAtObject = null;
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		
+		Debug.DrawRay(transform.position, transform.forward, Color.green);
+
+		if (Physics.Raycast(ray, out hit, 100))
+		{
+			_lookedAtObject = hit.collider.gameObject;
+			if (_lookedAtObject.transform.CompareTag("Crate"))
+			{
+				Timer.SetActive(true);
+			}
+		}
+		else
+		{
+			Timer.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/UpdateTimer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateTimer : MonoBehaviour
+{
+	public GameObject TimerMenu;
+	private bool _toggleTimerMenu;
+	private float _startTime = 0;
+	private float _elapsedTime = 0;
+	
+	void Start ()
+	{
+		TimerMenu.SetActive(_toggleTimerMenu);
+		_startTime = Time.time;
+	}
+	
+	void Update ()
+	{
+		if (_toggleTimerMenu)
+		{
+			_elapsedTime = Time.time - _startTime;
+			gameObject.GetComponentInChildren<Text>().text = "Elapsed time: " + _elapsedTime;
+		}
+
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			_toggleTimerMenu = !_toggleTimerMenu;
+			TimerMenu.SetActive(_toggleTimerMenu);
+		}
+	}
+}
