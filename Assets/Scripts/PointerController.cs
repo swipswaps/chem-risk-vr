49b9e85e807bbfc52478c2e60300219bf3a79438
Nikolay Ivanov <niki.ivanov98@gmail.com>
2018-11-21 19:16:22 +0100

    Integrated in-game ui with game objects
    This is only a template and an example of how it could work if we wanted
    to have in-game tablets or monitors on workstations and such for the
    player to interact with.

--- Assets/Scripts/PointerController.cs
+++ Assets/Scripts/PointerController.cs
@@ -1,28 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PointerController : MonoBehaviour
 {
-	public GameObject Timer;
+	//public GameObject Timer;
 	private GameObject _lookedAtObject = null;
-	
+	public Material DefaultObjectMaterial;
+	public Material HoverableObjectMaterial;
+	private MeshRenderer _meshRenderer;
+	private Image _image;
+	public LayerMask CatchItemLayer;
+	public LayerMask CatchButtonLayer;
+
 	// Update is called once per frame
 	void Update () {
 		Ray ray = new Ray(transform.position, transform.forward);
 		RaycastHit hit;
 		
-		Debug.DrawRay(transform.position, transform.forward, Color.green);
-
-		if (Physics.Raycast(ray, out hit, 100))
+		// Checking if the player is hovering over an item with the controller
+		if (Physics.Raycast(ray, out hit, 100, CatchItemLayer))
 		{
 			_lookedAtObject = hit.collider.gameObject;
-			if (_lookedAtObject.transform.CompareTag("Crate"))
+			_meshRenderer = _lookedAtObject.GetComponent<MeshRenderer>();
+			_meshRenderer.material = HoverableObjectMaterial;
+		}
+		// Checking if the player is hovering over a button with the controller
+		else if (Physics.Raycast(ray, out hit, 100, CatchButtonLayer))
+		{
+			_lookedAtObject = hit.collider.gameObject;
+			_image = _lookedAtObject.GetComponent<Image>();
+			_image.color = Color.red;
+			if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
 			{
-				Timer.SetActive(true);
+				_image.color = Color.green;
 			}
 		}
 		else
 		{
-			Timer.SetActive(false);
+			if (_image != null)
+			{
+				_image.color = Color.white;
+			}
+			if (_meshRenderer != null)
+			{
+				_meshRenderer.material = DefaultObjectMaterial;
+			}
 		}
 	}
 }
