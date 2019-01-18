5afba1cd08924a4a807da9cbe3f6d9f310738da0
Nikolay Ivanov <niki.ivanov98@gmail.com>
2019-01-19 00:12:57 +0100

    Made bin warm particle fire effect

--- Assets/Scripts/WasteBinController.cs
+++ Assets/Scripts/WasteBinController.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class WasteBinController : MonoBehaviour {
     public string BinType;
     public GameObject Leftovers;
-    
+    public GameObject FireParticles;
+    public GameObject SmokeParticles;
+    private bool _areParticlesInitiated = false;
+
     private void OnTriggerStay(Collider other)
     {
-        if ((other.tag == "Beaker" || other.tag == "Dropper"))
+        if (other.tag == "Beaker" || other.tag == "Dropper")
         {
+            if (_areParticlesInitiated == false && (BinType == "Warm" && other.gameObject.name == "Green Substance Beaker(Clone)" || other.gameObject.name == "Blue Substance Beaker(Clone)" || other.gameObject.name == "Purple Substance Beaker(Clone)"))
+            {
+                Instantiate(FireParticles, gameObject.transform);
+                _areParticlesInitiated = true;
+            }
+
+            if (_areParticlesInitiated == false && (BinType == "Warm" && other.gameObject.name == "Green Substance Beaker(Clone)" || other.gameObject.name == "Blue Substance Beaker(Clone)" || other.gameObject.name == "Purple Substance Beaker(Clone)"))
+            {
+                Instantiate(SmokeParticles, gameObject.transform);
+                _areParticlesInitiated = true;
+            }
             // We check if the right beaker's liquid is being thrown away
             Destroy(other.transform.GetChild(0).gameObject);
 
